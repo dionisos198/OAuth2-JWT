e718d34867e000425f07fd5cd8bc3a7a81a2703a
Jinu Yi <dionisos198@naver.com>
2023-10-07 04:22:52 +0900

    Feat : OAuth2 로그인

---
+++ src/test/java/com/OAuth2/oauth2Login/Oauth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OAuth2.oauth2Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
